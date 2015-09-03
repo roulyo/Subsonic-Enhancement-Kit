a5487c56f7ce766062bbec69b2039a8e016c8d14
Julien Fraisse <roulyo.42@gmail.com>
2015-09-03 16:20:48 +0200

    Fix last.fm link.

--- short-bio.js
+++ short-bio.js
@@ -26,7 +26,7 @@ function cutThatFuckingBio()
       if (!g_done && bio.innerHTML != "")
       {
         g_done = true; // Done at the beginning to avoid infinite recursion
-        var lastfmlnk = bio.innerHTML.match(/<a>.*>/gm);
+        var lastfmlnk = bio.innerHTML.match(/<.*>/gm);
 
         bio.innerHTML = bio.innerHTML.replace(/<.*>/gm, "");
         bio.innerHTML = bio.innerHTML.substr(0, BIO_LENGTH) + '... ' + lastfmlnk;
