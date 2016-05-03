e275be456cb34ec4323793839e98548ed03ff2b9
Julien Fraisse <roulyo.42@gmail.com>
2016-05-03 11:40:04 +0200

    Update fat-arts.js

--- fat-arts.js
+++ fat-arts.js
@@ -14,12 +14,15 @@ if (window.frameElement.name !== "main")
 
 function getFatArts()
 {
-    document.removeEventListener("DOMSubtreeModified", getFatArts, false);
+    if (document.getElementsByTagName("h1")[0].innerHTML === "Change cover art")
+    {
+        document.removeEventListener("DOMSubtreeModified", getFatArts, false);
 
-    var img = document.getElementsByClassName("search-result-link")[0];
-    img.href = img.href.replace(/300x300\//, "");
+        var img = document.getElementsByClassName("search-result-link")[0];
+        img.href = img.href.replace(/300x300\//, "");
 
-    document.addEventListener("DOMSubtreeModified", getFatArts, false);
+        document.addEventListener("DOMSubtreeModified", getFatArts, false);
+    }
 }
 
 document.addEventListener("DOMSubtreeModified", getFatArts, false);
