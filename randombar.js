7e122da79c4a443396530a09655f8ec0e54ac9b6
Julien Fraisse <roulyo.42@gmail.com>
2016-10-25 18:03:32 +0200

    Create randombar.js

---
+++ randombar.js
@@ -0,0 +1,64 @@
+// ==UserScript==
+// @name        Random bar
+// @namespace   roulyo
+// @include     http://subsonic.mogmi.fr/*
+// @version     1
+// @grant       none
+// ==/UserScript==
+
+if (window.frameElement.name !== "main")
+{
+    return;
+}
+
+const randomBarTemplate = "<form method=\"post\" action=\"randomPlayQueue.view?\">\
+        <table>\
+            <tbody><tr>\
+                <td>Shuffle play</td>\
+                <td>\
+                    <select name=\"size\">\
+                        <option value=\"10\">10 songs</option>\
+                        <option value=\"20\">20 songs</option>\
+                        <option value=\"30\">30 songs</option>\
+                        <option value=\"40\">40 songs</option>\
+                        <option value=\"50\" selected=\"\">50 songs</option>\
+                    </select>\
+                </td>\
+                <td style=\"display: none;\">from genre</td>\
+                <td style=\"display: none;\">\
+                    <select name=\"genre\">\
+                        <option value=\"any\">Any</option>\
+                    </select>\
+                </td>\
+                <td style=\"display: none;\">and year</td>\
+                <td style=\"display: none;\">\
+                    <select name=\"year\">\
+                        <option value=\"any\">Any</option>\
+                    </select>\
+                </td >\
+                <td style=\"display: none;\">in folder</td>\
+                <td style=\"display: none;\">\
+                    <select name=\"musicFolderId\">\
+                        <option value=\"4\">Music</option>        \
+                    </select>\
+                </td>\
+                <td>\
+                    <input value=\"RANDOMIZE!!1\" type=\"submit\">\
+                </td>\
+            </tr>            \
+        </tbody></table>\
+    </form>";
+
+var homeAnchor = document.getElementsByClassName("fa fa-home fa-lg icon");
+
+if (homeAnchor.length == 1)
+{
+    var randomBarNode = document.createElement("div");
+
+    randomBarNode.innerHTML = randomBarTemplate;
+
+    randomBarNode.style.fontSize = "15px";
+    randomBarNode.style.float = "right";
+
+    homeAnchor[0].parentElement.appendChild(randomBarNode);
+}
