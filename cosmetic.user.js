a4aac2fed53cad6a2599f4328f8bceafc768395e
Julien Fraisse <roulyo.42@gmail.com>
2017-11-30 14:32:25 -0500

    Update cosmetic.user.js

--- cosmetic.user.js
+++ cosmetic.user.js
@@ -6,40 +6,46 @@
 // @grant       none
 // ==/UserScript==
 
-function GridRescaling()
-{
-    var albumThumbnails = document.getElementsByClassName('albumThumb');
-
-    for (var i = 0; i < albumThumbnails.length; ++i)
-    {
-        var thumbnail = albumThumbnails[i];
-
-        thumbnail.style.paddingRight = '19px';
-        thumbnail.style.paddingBottom = '19px';
-    }
-}
-
-function RemoveWarnings()
-{
-    var warnings = document.getElementsByClassName('warning');
-
-    for (var i = 0; i < warnings.length; ++i)
-    {
-        var warning = warnings[i];
-
-        warning.parentElement.removeChild(warning);
-    }
-}
-
-function CSSMakeOver()
-{
-    GridRescaling();
-    RemoveWarnings();
-}
-
 (function() {
-    if ((document.readyState === "interactive" || document.readyState === "complete"))
+    'use strict';
+
+    if ((document.readyState !== "interactive" && document.readyState !== "complete"))
     {
-        CSSMakeOver();
+        return;
     }
+
+    function GridRescaling()
+    {
+        var albumThumbnails = document.getElementsByClassName('albumThumb');
+
+        for (var i = 0; i < albumThumbnails.length; ++i)
+        {
+            var thumbnail = albumThumbnails[i];
+
+            thumbnail.style.paddingRight = '19px';
+            thumbnail.style.paddingBottom = '19px';
+        }
+    }
+
+    function RemoveWarnings()
+    {
+        var warnings = document.getElementsByClassName('warning');
+
+        for (var i = 0; i < warnings.length; ++i)
+        {
+            var warning = warnings[i];
+
+            warning.parentElement.removeChild(warning);
+        }
+    }
+
+    function Facelift()
+    {
+        GridRescaling();
+        RemoveWarnings();
+    }
+
+
+    Facelift();
+
 })();
