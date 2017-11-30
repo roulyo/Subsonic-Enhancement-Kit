046d1dd59af0498b3f72e1d5537912eb2773d729
Julien Fraisse <roulyo.42@gmail.com>
2017-11-30 14:49:50 -0500

    Update lyrics.user.js

--- lyrics.user.js
+++ lyrics.user.js
@@ -6,34 +6,42 @@
 // @grant       none
 // ==/UserScript==
 
-document.addEventListener("DOMSubtreeModified", swapLyricsLink, false);
+(function() {
+    'use strict';
 
-function swapLyricsLink()
-{
-    document.removeEventListener("DOMSubtreeModified", swapLyricsLink, false);
-    
-    if (window.frameElement.name !== "right")
+    if (window.frameElement && window.frameElement.name !== "right")
     {
         return;
     }
 
-    var lyricsLinks = document.getElementsByClassName("now-playing-lyrics");
+    console.log("aunching lyrics");
 
-    for (var i = 0; i < lyricsLinks.length; ++i)
+    function swapLyricsLink()
     {
-        var lyricsLink = lyricsLinks[i];
-        var grandParent = lyricsLink.parentElement.parentElement;
-        var title = encodeURIComponent(grandParent.getElementsByClassName("now-playing-title")[0].innerHTML);
-        var artist = encodeURIComponent(grandParent.getElementsByClassName("now-playing-artist")[0].innerHTML);
-        var newLyricsLink = document.createElement("a");
+        document.removeEventListener("DOMSubtreeModified", swapLyricsLink, false);
 
-        newLyricsLink.href = "http://lyrics.mogmi.fr/artist/" + artist + "/track/" + title;
-        newLyricsLink.innerText = "Lyrics";
-        newLyricsLink.className = "now-playing-lyrics clickable";
-        newLyricsLink.target = "_blank";
-        
-        lyricsLink.parentNode.replaceChild(newLyricsLink, lyricsLink);
+        var lyricsLinks = document.getElementsByClassName("now-playing-lyrics");
+
+
+        for (var i = 0; i < lyricsLinks.length; ++i)
+        {
+            var lyricsLink = lyricsLinks[i];
+            var grandParent = lyricsLink.parentElement.parentElement;
+            var title = encodeURIComponent(grandParent.getElementsByClassName("now-playing-title")[0].innerHTML.replace(/\s+/g, "_"));
+            var artist = encodeURIComponent(grandParent.getElementsByClassName("now-playing-artist")[0].innerHTML.replace(/\s+/g, "_"));
+            var newLyricsLink = document.createElement("a");
+
+            newLyricsLink.href = "http://lyrics.wikia.com/wiki/" + artist + ":" + title;
+            newLyricsLink.innerText = "Lyrics";
+            newLyricsLink.className = "now-playing-lyrics clickable";
+            newLyricsLink.target = "_blank";
+
+            lyricsLink.parentNode.replaceChild(newLyricsLink, lyricsLink);
+        }
+
+        document.addEventListener("DOMSubtreeModified", swapLyricsLink, false);
     }
 
     document.addEventListener("DOMSubtreeModified", swapLyricsLink, false);
-}
+
+})();
