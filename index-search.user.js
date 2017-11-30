72745223552f74813b60e1df97ddfae374ed20a1
Julien Fraisse <roulyo.42@gmail.com>
2017-11-30 14:33:08 -0500

    Update index-search.user.js

--- index-search.user.js
+++ index-search.user.js
@@ -6,32 +6,34 @@
 // @grant       none
 // ==/UserScript==
 
-function addLinkToDiv()
-{
-    var index = document.getElementsByClassName("left-menu-item left-index-shortcut-artist ellipsis");
-
-    console.log("Found "+ index.length + " entries");
-
-    for (var i = 0; i < index.length; ++i)
-    {
-        var entry = index[i];
-        var span = entry.getElementsByClassName("ellipsis")[0];
-        var link = document.createElement("a");
-
-        link.href = "javaScript:void(0);";
-        link.class = span.class;
-        link.innerHTML = span.textContent;
-        link.style.textDecoration = "none";
-
-        span.parentNode.replaceChild(link, span);
-    }
-}
-
 (function() {
     'use strict';
 
-    if (window.frameElement && window.frameElement.name === "left")
+    if (window.frameElement && window.frameElement.name !== "left")
     {
-        addLinkToDiv();
+        return;
     }
+
+    function addLinkToDiv()
+    {
+        var index = document.getElementsByClassName("left-menu-item left-index-shortcut-artist ellipsis");
+
+        for (var i = 0; i < index.length; ++i)
+        {
+            var entry = index[i];
+            var span = entry.getElementsByClassName("ellipsis")[0];
+            var link = document.createElement("a");
+
+            link.href = "javaScript:void(0);";
+            link.class = span.class;
+            link.innerHTML = span.textContent;
+            link.style.textDecoration = "none";
+
+            span.parentNode.replaceChild(link, span);
+        }
+    }
+
+    addLinkToDiv();
+
 })();
+
